a704af90f4f5b8ca78503b7465efd18fb21764a2
luke bertram and shane ryan <luke@>
2017-10-25 11:18:12 -0700

    add ay to single letter words that are vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 //back-end business time
 var vowels = ['a','e','i','o','u']
+var isVowel = function(letter){
+
+  return true;
+};
+
 var latinize = function(input){
-  var translated = input;
+  var translated;
+  var firstLetter = input.charAt(0);
+
+  if (isVowel(firstLetter)){
+    translated = input.concat("way");
+  }
   return translated;
-}
+};
+
 
 //front-end funky town
 $(document).ready(function(){
